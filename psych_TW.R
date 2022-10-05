236b371c595c9312daa1b41b55b887b5c964cfa0
tim9800 <twon8031@uni.sydney.edu.au>
2022-10-05 22:29:42 +1100

    Testing mixedCor

--- psych_TW.R
+++ psych_TW.R
@@ -1,2 +1,12 @@
+# Development
 library(devtools)
 use_r("fa.pooled.r")
+
+# Mucking around with mixedCor
+library(psych)
+
+data(bfi)
+r1 <- mixedCor(bfi, p=1:5, c=28, d=26)
+r1$rho
+r2 <- mixedCor(bfi, p=NULL, c=NULL, d=NULL)
+r2$rho
